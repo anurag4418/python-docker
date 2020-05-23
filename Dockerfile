def3b4868fe1113284acdfc46222828b991915e2
root <root@docker.us-central1-a.c.northern-eon-273717.internal>
2020-05-23 13:36:39 +0000

    changes in dockerfile & python script

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
-FROM ubuntu
-RUN apt-get update
-RUN apt-get install python
+FROM ubuntu:16.04
+RUN apt-get update && apt-get install -y \
+    software-properties-common
+RUN add-apt-repository universe
+RUN apt-get update && apt-get install -y \
+    python3.4 \
+    python3-pip    
+RUN python3 -m pip install --upgrade pip
+RUN pip install mysql-connector-python
 COPY . /opt/app
-CMD[python insert-scripy.py]
+CMD [python insert-scripy.py]
--- insert-script.py
+++ insert-script.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from datetime import date
 mydb = mysql.connector.connect(
-    host = "localhost",
+    host = "172.17.0.2",
     user= "root",
     passwd = "password",
     database = "cicd"
@@ -12,10 +12,12 @@ mycursor = mydb.cursor();
 sql = "INSERT INTO USER_DETAILS (firstname, lastname,email,username,password,lastupdated) VALUES (%s,%s,%s,%s,%s,%s)"
 val =[
 ('firstname1','lastname1','email1','admin1','admin1',date.today()),
- 'firstname2','lastname2','email2','admin2','admin2',date.today()),
- 'firstname3','lastname3','email3','admin3','admin3',date.today()),
- 'firstname4','lastname4','email4','admin4','admin4',date.today()),
- 'firstname5','lastname5','email5','admin5','admin5',date.today()),
+('firstname2','lastname2','email2','admin2','admin2',date.today()),
+('firstname3','lastname3','email3','admin3','admin3',date.today()),
+('firstname4','lastname4','email4','admin4','admin4',date.today()),
+('firstname5','lastname5','email5','admin5','admin5',date.today()),
+('firstname6','lastname6','email6','admin6','admin6',date.today()),
+('firstname7','lastname7','email7','admin7','admin7',date.today()),
 ]
 
 mycursor.executemany(sql,val)
